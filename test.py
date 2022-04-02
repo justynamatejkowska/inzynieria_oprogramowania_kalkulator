e8292edbe9f6efa8cfaab72470b40792678094bd
Justyna <j.matejkowska@gmail.com>
2022-04-02 20:33:55 +0200

    zmieniono zmienna z b na c

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ def  hello(name):
 	return "Hello" + str(name)
 print ("jak miło cie widziec :)")
 
-def odejmij(a,b):
-	return a-b
+def odejmij(a,c):
+	return a-c
 
 def dodaj(a,b):
 	wynik = float(a) + float(b)
@@ -14,9 +14,4 @@ pierwsza = input()
 druga = input()
 print (dodaj(pierwsza, druga))
 
-def add_num(a,b):#function for multiplication
-    multiply=a*b;
-    return multiply; #return value
-num1=int(input("input the number one: ")) #input from user for num1
-num2=int(input("input the number one: ")) #input from user for num2
-print("The product is",add_num(num1,num2)) #call te function 
+
