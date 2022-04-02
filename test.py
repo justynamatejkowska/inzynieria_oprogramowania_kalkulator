158eeefb8eee47266cc3ae3b997c3afb1232d201
Justyna <j.matejkowska@gmail.com>
2022-04-02 19:47:32 +0200

    dodano mnozenie

--- test.py
+++ test.py
@@ -14,5 +14,9 @@ pierwsza = input()
 druga = input()
 print (dodaj(pierwsza, druga))
 
-#to będzie plik na którym bedziemy pracowali :)
-print ("Hello world")
+def add_num(a,b):#function for multiplication
+    multiply=a*b;
+    return multiply; #return value
+num1=int(input("input the number one: ")) #input from user for num1
+num2=int(input("input the number one: ")) #input from user for num2
+print("The product is",add_num(num1,num2)) #call te function 
